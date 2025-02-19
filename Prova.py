52377a67b3401f466fee449bbb1fc43f9079304d
Jeferson Aguero <jeferson.aguero1@gmail.com>
2025-02-19 19:59:05 -0300

    commit inicial

---
+++ Prova.py
@@ -0,0 +1,4 @@
+base = float(input("Digite o valor da base do retangulo: "))
+altura=float(input("Digite o valor da altura do retangulo: "))
+area = base * altura
+print(f"A área do retângulo é: {area}")
